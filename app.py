6409e2c8a4d4375b02cbf815ed46ef352d71f46d
VaibhavK <vlr33l2099@gmail.com>
2025-05-04 13:29:10 +0530

    trying to fetch ip of deployed streamlit app

--- app.py
+++ app.py
@@ -87,6 +87,14 @@ import streamlit as st
 from utils.auth import verify_user
 from utils.db import users_col
 
+import requests
+
+# Fetch the public IP
+ip = requests.get("https://api.ipify.org").text
+
+# Display it (or log it)
+st.write(f"Streamlit App's Outbound IP: **{ip}**")
+
 st.set_page_config(page_title="Kommuniti App", page_icon="🔧")
 
 st.title("🔐 Login to Kommuniti Dashboard")
